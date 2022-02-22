a9e985ff959b18d851829edad56d862f02cf7923
William Marchant <william.marchant2@outlook.es>
2022-02-22 22:25:13 +0100

    first commit

---
+++ app/src/main/java/com/local/marchant/app/ui/PlayFragment.java
@@ -0,0 +1,101 @@
+package com.local.marchant.app.ui;
+
+import android.annotation.SuppressLint;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.local.marchant.app.R;
+import com.local.marchant.app.view.CanvasView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PlayFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PlayFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private Canvas canvas;
+    private RelativeLayout screen;
+    private Paint paint;
+
+    public PlayFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PlayFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PlayFragment newInstance(String param1, String param2) {
+        PlayFragment fragment = new PlayFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_play, container, false);
+
+        getActivity().setTitle("Simon Game | Play");
+
+        View canvasV = (CanvasView) root.findViewById(R.id.canvas);
+
+        canvasV.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float x = event.getX(), y = event.getY();
+                int X = (int) x / (canvasV.getWidth() / 2), Y = (int) y / (canvasV.getHeight() / 2);
+                String color = "";
+
+                if(X == 0 && Y == 0) color = "GREEN";
+                else if(X == 1 && Y == 0) color = "RED";
+                else if(X == 0 && Y == 1) color = "YELLOW";
+                else if(X == 1 && Y == 1) color = "BLUE";
+
+                Log.i("TAG", "Touched color: " + color);
+
+                return false;
+            }
+        });
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/local/marchant/app/view/CanvasView.java
@@ -0,0 +1,43 @@
+package com.local.marchant.app.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CanvasView extends View {
+
+    public CanvasView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float width = getWidth() / 2;
+        float height = getHeight() / 2;
+
+        Paint paint = new Paint();
+
+        paint.setColor(Color.GREEN);
+        canvas.drawRect(0, 0, width, height, paint);
+
+        paint.setColor(Color.RED);
+        canvas.drawRect(width, 0, getWidth(), height, paint);
+
+        paint.setColor(Color.YELLOW);
+        canvas.drawRect(0, height, width, getHeight(), paint);
+
+        paint.setColor(Color.BLUE);
+        canvas.drawRect(width, height, getWidth(), getHeight(), paint);
+    }
+
+    public void clickArea(){
+
+    }
+}
